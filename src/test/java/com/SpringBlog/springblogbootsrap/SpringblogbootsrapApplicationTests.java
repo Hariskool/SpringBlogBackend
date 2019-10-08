50957e05698da5d0f6ee3e45eac7b5a7cd26509c
Prince Harry <kool345@yahoo.com>
2019-10-08 11:50:12 +0500

    adding java class

---
+++ src/test/java/com/SpringBlog/springblogbootsrap/SpringblogbootsrapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SpringBlog.springblogbootsrap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringblogbootsrapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
